616300b00122448e78c2b24681136438fff2f241
sazzad ul alam Shawon <93247296+sazzadulalambd@users.noreply.github.com>
2022-03-13 10:01:15 +0600

    fast commit

---
+++ Project/view/s_admin_reg02.php
@@ -0,0 +1,65 @@
+<?php include "./nav_ber_ad.php"?>
+<?php session_start();?>
+<html>
+<body>
+<center>
+<img src="../img/title_logo.png" width="88" height="80">
+<h3>Admin Join BEPARTNER    2/3</h3>
+
+<form action="" method="post">
+
+<table>
+<tr>
+<td>NID:</td>
+<td><input type="text" name="Nname" placeholder="10 digit number" value="<?php echo  $_SESSION["Nname"];  ?>" ></td>
+</tr>
+
+<tr>
+<td>Phone number:</td>
+<td><input type="tel" id="phone" name="phone" placeholder="01998888224" value="<?php echo  $_SESSION["phone"];  ?>"></td>
+</tr>
+
+<tr>
+<td>Address:</td>
+<td><input type="text" name="Address" value="<?php echo  $_SESSION["Address"];  ?>"></td>
+</tr>
+
+<tr>
+<td>Security key:</td>
+<td><input type="key" name="Skey" value="<?php echo  $_SESSION["Skey"];  ?>" ></td>
+</tr>
+
+<tr>
+  <td>Gender:</td>
+ <td><input type="radio" name = "gender" value="r1">Male
+ <input type="radio" name = "gender" value="r2">Female
+  <input type="radio" name = "gender" value="r3">Other</td>
+</tr>
+
+<tr>
+<td>Date of birth:</td>
+<td><input type="date" name="age" value="<?php echo  $_SESSION["age"];  ?>"></td>
+</tr>
+
+
+<tr> 
+<td>
+</td>
+</tr>
+</table>
+
+<input type="submit" value="Previous" name = "previous">
+<input type="submit" value="Continue" name="ad_next1">
+<input type="reset" value="Reset">
+
+<p> Already have an account? <a href="sign_in_admin.php">Sign In</a>
+<br> <br> <br> <br>
+
+<?php include "../control/ad_process_join2.php"?>
+</center>
+</body>
+</html>
+
+
+
+<?php include "./footer.php"?>
---
+++ Project/view/s_admin_reg03.php
@@ -0,0 +1,40 @@
+<?php include "./nav_ber_ad.php"?>
+
+<html>
+<body>
+<center>
+<img src="../img/title_logo.png" width="88" height="80">
+<h3>Admin Join BEPARTNER   3/3</h3>
+
+<form action="" method="post">
+
+<table>
+<tr>
+<td>Password:</td>
+<td><input type="password" name="pwd"></td>
+</tr>
+
+<tr>
+<td>Confirm password:</td>
+  <td><input type="password" name="pwd2"></td>
+</tr>
+<tr>
+<td><input type="checkbox" name="checkbox">I agree to BEPARTNER's Terms of service
+</tr>
+
+
+</table>
+
+<input type="submit" value="Previous" name = "previous">
+<input type="submit" value="Join" name="join">
+<input type="reset"  value="Reset">
+
+<p> Already have an account? <a href="sign_in_admin.php">Sign In</a>
+<br> <br> <br> <br>
+<?php include "../control/ad_process_join3.php"?>
+</center>
+</body>
+</html>
+
+
+<?php include "./footer.php"?>
